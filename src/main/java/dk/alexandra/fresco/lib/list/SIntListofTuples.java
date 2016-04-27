b2fe5ebea8dde402a4b547dcc24c54440f204f62
Morten V. Christiansen <morten.v.christiansen@alexandra.dk>
2016-04-27 15:58:37 +0200

    new file

---
+++ src/main/java/dk/alexandra/fresco/lib/list/SIntListofTuples.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dk.alexandra.fresco.lib.list;
+
+/**
+ * @author mortenvchristiansen
+ *
+ */
+public class SIntListofTuples {
+
+	/**
+	 * id is first element in tuple
+	 */
+	public SIntListofTuples() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
