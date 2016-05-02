327a9951d3aa41e108de05a2e8c0a1bc9f8b7c12
Morten V. Christiansen <morten.v.christiansen@alexandra.dk>
2016-05-02 16:31:14 +0200

    nn

--- src/main/java/dk/alexandra/fresco/lib/list/SIntListofTuples.java
+++ src/main/java/dk/alexandra/fresco/lib/list/SIntListofTuples.java
@@ -3,17 +3,45 @@
  */
 package dk.alexandra.fresco.lib.list;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import dk.alexandra.fresco.framework.value.SInt;
+
 /**
  * @author mortenvchristiansen
  *
  */
 public class SIntListofTuples {
-
+  private List<SInt[]> theData = new ArrayList<SInt[]>();
 	/**
 	 * id is first element in tuple
 	 */
-	public SIntListofTuples() {
-		// TODO Auto-generated constructor stub
+  	public final int rowWidth;
+  	
+  	public SIntListofTuples add(SInt[] row){
+  	    if (row.length!=rowWidth)
+  	    	throw new RuntimeException("Row width wrong. Should be "+rowWidth);
+  	    theData.add(row);	
+  		return this;
+  	}
+  	
+  	public SIntListofTuples remove(int index){
+  	    theData.remove(index);	
+  		return this;
+  	}
+  	
+  	public SInt[] get(int index){
+  		return theData.get(index);
+  	}
+  	
+  	public List<SInt[]> getReadOnlyList(){
+  		return Collections.unmodifiableList(theData);
+  	}
+  	
+	public SIntListofTuples(int rowWidth) {
+		this.rowWidth=rowWidth;
 	}
 
 }
---
+++ src/main/java/dk/alexandra/fresco/lib/list/SIntListofTuplesTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package dk.alexandra.fresco.lib.list;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import dk.alexandra.fresco.framework.value.SInt;
+
+/**
+ * @author mortenvchristiansen
+ *
+ */
+public class SIntListofTuplesTest {
+
+	
+	private final int[][] newData={{0,1,2},{1,2,3},{2,3,4},{3,4,5},{4,5,6}};
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
